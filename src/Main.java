78e572b9361601ee7e07ec09aeff33438775894e
Juan Hernandez Puebla <al222010053@gmail.com>
2022-11-01 13:00:13 -0600

    Ejercicios tema 3, primera y segunda parte

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        //Primera parte
+        int res = suma(5,10,15);
+        System.out.println(res);
+
+
+        //Segunda parte
+        Coche miCoche = new Coche();
+        miCoche.IncrementarPuerta();
+        miCoche.IncrementarPuerta();
+        miCoche.IncrementarPuerta();
+        miCoche.IncrementarPuerta();
+        System.out.println("Mi coche tiene "+miCoche.puerta+" puertas");
+    }
+
+    //Funcion primera parte
+    public static int suma(int a, int b, int c) {
+        return a+b+c;
+    }
+}
+//Clase segunda parte
+class Coche{
+    public int puerta = 0;
+    public Coche(){}
+    public void IncrementarPuerta(){
+        this.puerta++;
+    }
+}
+
